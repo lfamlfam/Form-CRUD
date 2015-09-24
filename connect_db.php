1a9f1002d7a39bc93c94150140136f76ed250fc5
Luiz Fernando <lfamlfam@gmail.com>
2015-09-23 22:14:55 -0300

    exemplo de require

--- connect_db.php
+++ connect_db.php
@@ -5,7 +5,9 @@ $user = "TSI@i9yueekhr9.database.windows.net";
 $password = "SistemasInternet123";
 $dsn = "Driver={SQL Server};Server=$dbhost;Port=1433;Database=$db;";
                
-$connect = odbc_connect($dsn,$user,$password);
+$connect = odbc_connect($dsn,
+						$user,
+						$password);
 
 $q = odbc_exec($connect,'SELECT * FROM Cliente');
 echo "<pre>";
---
+++ templates/login_form_template.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>
+			Exemplo de login
+		</title>
+	</head>
+	<body>
+		<br><br><br>
+		<center>
+			<form method='post'>
+				Login <input type='text'
+						name='login'><br><br>
+				Senha <input type='password'
+						name='senha'><br><br>
+				<input type='submit' 
+						name='entrar'
+						value='Entrar'>
+			</form>
+		</center>
+	</body>
+</html>
